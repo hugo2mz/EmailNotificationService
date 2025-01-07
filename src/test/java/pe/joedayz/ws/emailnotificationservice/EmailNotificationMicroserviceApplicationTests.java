c694b49005b2f7da736eb7993bf5c4f3e6285d7a
Jose Amadeo Diaz Diaz <jose.diaz@joedayz.pe>
2025-01-06 21:56:22 -0500

    first version

---
+++ src/test/java/pe/joedayz/ws/emailnotificationservice/EmailNotificationMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.joedayz.ws.emailnotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailNotificationMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
